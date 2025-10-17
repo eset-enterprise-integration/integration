98a4316365fd665267e3880e0f0dcf7dda4f29ec
eset-enterprise-integration <eset-enterprise-integration@eset.com>
2025-10-17 14:55:05 +0200

    Update main.py

--- integration/main.py
+++ integration/main.py
@@ -187,7 +187,7 @@ class ServiceClient:
                 )
                 is_obtained = True if data and any(data.get(v) for v in ("createdDetections", "incidents")) else False
                 logging.info(
-                    f"Service call {data_endpoint} response data is {'obtained' if is_obtained else 'empty'}"
+                    f"Service call {data_endpoint} response data is {'obtained' if is_obtained else f'empty: {data}'}"
                 )
                 return data
 
@@ -196,4 +196,5 @@ class ServiceClient:
             logging.error(f"Error in running service call: {e}")
 
         return None
+
 
